cf57491c80643d894c672a663b487450633436eb
Magicking <s@6120.eu>
2018-03-21 04:45:38 +0100

    Add random generated private key and working solo clique based miner

--- sharding/docker_files/run.sh
+++ sharding/docker_files/run.sh
@@ -7,6 +7,41 @@ password_file=/signer.password
 GENESIS_PATH=/genesis.json
 CORS_DOMAIN="*"
 
+modules="eth,shh,web3,admin,debug,miner,personal,txpool"
+gethcmd="/usr/local/bin/geth --datadir ${DATA_DIR}"
+
+echo "${PASSWORD}" > "${password_file}"
+
+if [ x"${NETWORK_ID}" == x ]; then
+  NETWORK_ID=1092
+fi
+
+import_genesis() {
+  if [ -d "${DATA_DIR}"/geth/chaindata ]; then
+    echo "Not importing genesis"
+    return
+  fi
+  file_size=$(wc -c < "$GENESIS_PATH")
+  if [ $file_size -gt 1 ] && \
+      $gethcmd init "${GENESIS_PATH}"; then
+      echo "Genesis imported"
+  else
+      echo "Could not import init, bad genesis.json"
+      exit 1
+  fi
+}
+
+import_key() {
+  if [ x"${PRIVATE_KEY}" == "x" ]; then
+    echo "Not importing private key"
+    return
+  fi
+  pkey_file=$(mktemp)
+  echo "${PRIVATE_KEY}" > "${pkey_file}"
+  $gethcmd --datadir "${DATA_DIR}" --password "${password_file}" account import "${pkey_file}"
+  rm "${pkey_file}"
+}
+
 usage ()
 {
   echo "/run.sh proposer | collator | full-node"
@@ -15,6 +50,7 @@ usage ()
 
 proposer ()
 {
+  proposerip=`nslookup "${PROPOSER_NAME}" 2>/dev/null | grep 'Address 1' | awk '{ print $3 }'`
   echo "Not yet implemented"
   exit 1
 }
@@ -30,65 +66,35 @@ collator ()
 
 fullnode ()
 {
-  echo "Not yet implemented"
-  exit 1
+  import_key
+  import_genesis
+
+  # Removing the 0x part
+  ETHERBASE=`echo -n "${ETHERBASE}" | sed "s:\(0x\|^\)\(.*\):\2:"`
+  keystore_file=`ls ${DATA_DIR}/keystore | grep -i "${ETHERBASE}"`
+  if [ x"${keystore_file}" == "x" ]; then
+    echo "Private key doesn't match Etherbase, not lauching Clique miner"
+    exit 1
+  fi
+
+  exec $gethcmd \
+   --etherbase "${ETHERBASE}" \
+   --unlock "${ETHERBASE}" \
+   --mine --minerthreads 1 \
+   --networkid "${NETWORK_ID}" \
+   --maxpeers 100 \
+   --password ${password_file} \
+   --ws \
+   --wsaddr "0.0.0.0" \
+   --wsapi "${modules}" \
+   --wsorigins "${CORS_DOMAIN}"
 }
 
 case "$1" in 
   proposer) proposer ;;
-  collator) ;;
-  full-node) ;;
+  collator) collator ;;
+  full-node) fullnode ;;
   *)
   usage
 ;;
 esac
-
-if [ x"${NETWORK_ID}" == x ]; then
-  NETWORK_ID=29957
-fi
-
-gethcmd="/usr/local/bin/geth --datadir ${DATA_DIR}"
-
-# TODO: 
-proposerip=`nslookup "${PROPOSER_NAME}" 2>/dev/null | grep 'Address 1' | awk '{ print $3 }'`
-
-echo "${PASSWORD}" > "${password_file}"
-
-import_genesis() {
-  file_size=$(wc -c < "$GENESIS_PATH")
-  if [ $file_size -gt 1 ] && \
-      $gethcmd init "${GENESIS_PATH}"; then
-      echo "Genesis imported"
-  else
-      echo "Could not import init, bad genesis.json"
-      exit 1
-  fi
-}
-
-import_key() {
-  pkey_file=$(mktemp)
-  echo "${PRIVATE_KEY}" > "${pkey_file}"
-  $gethcmd --datadir "${DATA_DIR}" --password "${password_file}" account import "${pkey_file}"
-  rm "${pkey_file}"
-}
-
-if [ x"${PRIVATE_KEY}" != "x" ]; then
-  echo "Importing private key"
-  import_key
-fi
-
-if ! [ -d "${DATA_DIR}"/geth/chaindata ]; then
-  echo "Importing genesis"
-  import_genesis
-fi
-
-modules="eth,shh,web3,admin,debug,miner,personal,txpool"
-
-echo exec $gethcmd \
- --networkid "${NETWORK_ID}" \
- --maxpeers 100 \
- --password ${password_file} \
- --ws \
- --wsaddr "0.0.0.0" \
- --wsapi "${modules}" \
- --wsorigins "${CORS_DOMAIN}"
